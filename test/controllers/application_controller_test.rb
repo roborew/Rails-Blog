c21b68094dc9a618e64bb9d4250a00bbbc723ac4
Robin Andrew <robinrew@gmail.com>
2024-08-29 12:58:47 +0100

    Initial commit and Post model added.

---
+++ test/controllers/application_controller_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'minitest/autorun'
+
+class ApplicationControllerTest < Minitest::Test
+  def setup
+    # Do nothing
+  end
+
+  def teardown
+    # Do nothing
+  end
+
+  def test
+    skip 'Not implemented'
+  end
+end
