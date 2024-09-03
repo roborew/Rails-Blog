4dadc010f0a3b4ab47e3738f499b10fb9f0e3bfd
Robin Andrew <robinrew@gmail.com>
2024-09-03 13:13:05 +0100

    Update fixtures and tests to include draft, published, and scheduled articles.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_30_183506) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_02_120511) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "published_at"
   end
 
   create_table "users", force: :cascade do |t|
--- test/models/article_test.rb
+++ test/models/article_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class ArticleTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Draft article returns true for a new article" do
+    assert articles(:draft).draft?
+  end
+
+  test "draft? returns false for published article" do
+    refute articles(:published).draft?
+  end
+
+  test "draft? returns false for scheduled article" do
+    refute articles(:scheduled).draft?
+  end
+
+  test "Published? returns true for published article" do
+    assert articles(:published).published?
+  end
+
+  test "Published? returns false for draft article" do
+    refute articles(:draft).published?
+  end
+
+  test "Published? returns false for scheduled article" do
+    refute articles(:scheduled).published?
+  end
+
+  test "Scheduled? returns true for scheduled article" do
+    assert articles(:scheduled).scheduled?
+  end
+
+  test "Scheduled? returns false for draft article" do
+    refute articles(:draft).scheduled?
+  end
+
+  test "Scheduled? returns false for published article" do
+    refute articles(:published).scheduled?
+  end
 end
