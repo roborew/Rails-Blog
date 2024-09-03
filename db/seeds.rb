580cda2e1ca48153731f443fddb4ad859865a0b2
Robin Andrew <robinrew@gmail.com>
2024-09-03 21:31:43 +0100

    Update seeds.rb to create articles with dynamic content and publication dates.

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,7 @@ user.update!(
   password_confirmation: "password",
 )
 
-# 100.times do |i|
-#   Article.create!(
-#     title: "Article #{i}",
-#     content: "Content #{i}",
-#     published_at: Time.zone.now
-#   )
-# end
+100.times do |i|
+  article = Article.where(title: "Article #{i}").first_or_initialize
+  article.update!(content: "Content #{i}", published_at: Time.current)
+end
